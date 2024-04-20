ffc429eaa04ab9ac62f84909da9741df3f11c509
Anh Phuong Nguyen <anhphuong.nguyenquynh02@gmail.com>
2024-04-20 23:13:21 +0700

    edit gru4rec file remove custome opt

--- modules/gru4rec.py
+++ modules/gru4rec.py
@@ -11,7 +11,7 @@ import os.path
 orig_cwd = os.getcwd()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.environ['THEANORC'] = '.theanorc_gru4rec' #Only affects the actual settings if theano was not imported before this point (by any module)
-import custom_opt
+##import custom_opt
 import datatools as datatools
 import theano
 from theano import tensor as T
